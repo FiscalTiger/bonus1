9f51226a87a81064f0620b062ea03274979c3f49
FiscalTiger <apouleson@gmail.com>
2016-03-26 19:10:30 -0400

    got ajax working to retrieve search suggestions

--- template/bonus_one.js
+++ template/bonus_one.js
@@ -11,7 +11,28 @@
 // $.html(), $.text(), etc.
 // keyup events could be helpful to get value of field as the user types
 
-(function() {
-	// Magic!
-	console.log('Keepin\'n it clean with an external script!');
-})();
+// (function() {
+// 	var json;
+// 	$.getJSON("http://www.mattbowytz.com/simple_api.json", "data=all", function(data) {
+// 		json = data;
+// 		console.log(json);
+// 	});
+//
+// 	console.log(json);
+// 	console.log('Keepin\'n it clean with an external script!');
+// })();
+
+var search_suggestions;
+
+function predictive_search() {
+	console.log(search_suggestions);
+}
+
+$(function () {
+	$.getJSON("http://www.mattbowytz.com/simple_api.json", "data=all", function(data) {
+		search_suggestions = data.data.interests;
+		search_suggestions.push.apply(search_suggestions, data.data.programming);
+		console.log("Suggestions ready");
+	});
+	$(".flexsearch-input").keyup(predictive_search);
+});
